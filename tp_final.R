213b7175ef2271fee1d7fa3437d1faee9c170962
lucafocaia <lucafocaia.12@gmail.com>
2023-11-23 13:05:48 -0300

    23/11/23

--- tp_final.R
+++ tp_final.R
@@ -3,8 +3,12 @@
  ## Analisis de las concentraciones de CO2 en Hawaii ##
 
 #Leo el archivo ascii y cargo los datos
+ #CASA
 archivo <- "~/PracticasLabo/TP_Final/co2_daily_mlo.csv" 
 archivo1 <- "~/PracticasLabo/TP_Final/co2_mm_mlo.csv"
+ #FACU
+archivo <- "~/Documentos/Labo_Luca/TP_Final/co2_daily_mlo.csv" 
+archivo1 <- "~/Documentos/Labo_Luca/TP_Final/co2_mm_mlo.csv"
 #Direcciones de los archivos a usar
 
 datos <- read.csv(archivo, skip = 32)
@@ -18,11 +22,6 @@ datos1 <- read.csv(archivo1, skip = 40)
 head(datos1)
 tail(datos1) #Datos mensuales desde Marzo de 1958 hasta Septiembre de 2023
 
-#Quiero aprovechar los datos desestacionalizados pero solamente estan en el
- #archivo con datos mensuales, entonces voy a seleccionar el periodo 
- #Enero 1975 - Diciembre 2022, recorto esos datos que me interesan y los pego
- #en los diarios
-
 #Recorto mis datos
 datos <- datos[datos$Anio %in% 1984:2022,]
 datos1 <- datos1[datos1$year %in% 1984:2022,]
@@ -31,7 +30,7 @@ datos1 <- datos1[datos1$year %in% 1984:2022,]
 #Armo un nuevo df y lo voy llenando
  #Anios
 anios <- rep(1984:2022, each = 365)
- 
+
  #Meses
 ene <- rep(1, 31)
 feb <- rep(2, 28)
@@ -111,6 +110,10 @@ for (i in 1:length(datos$Fecha)) {
     datos$CO2_ppm[datos$Fecha == fecha]
 }
 
+#Quiero aprovechar los datos desestacionalizados pero solamente estan en el
+#archivo con datos mensuales, entonces voy a seleccionar el periodo 
+#Enero 1984 - Diciembre 2022, recorto esos datos que me interesan y los pego
+#en los diarios
 #Agrego en "datos" una columna con los datos desestacionalizados
 Desestacionalizado = c()
 for (i in 1:length(anios1)) {
@@ -131,49 +134,85 @@ for (i in 1:length(anios1)) {
 #Agrego a mis datos la columna con los datos desestacionalizados
 datos_nuevo = cbind(datos_nuevo, Desestacionalizado)
 
-#Selecciono los datos de 2019 a 2021 para armar el grafico solicitado
-datos_periodo <- datos_nuevo[datos_nuevo$Anio %in% 2019:2021,]
+#Selecciono los datos de 2020 a 2021 para armar el grafico solicitado
+datos_periodo <- datos_nuevo[datos_nuevo$Anio %in% 2020:2021,]
 
 #Calculo los promedios mensuales y semanales
  #PROMEDIO MENSUAL
-anios_periodo <- c(2019:2021)
-media_periodo = c()
+anios_periodo <- c(2020:2021)
+media_mensual_periodo = c()
 for (i in 1:length(anios_periodo)) {
   for (j in 1:length(meses1)) {
     anio <- anios_periodo[i]
     datos_anio <- datos_periodo[datos_periodo$Anio == anio,]
     datos_mes <- datos_anio[datos_anio$Mes == j,]
     media_mes <- round(mean(datos_mes$CO2_ppm, na.rm = T),2)
-    media_periodo[i] <- media_mes
+    media_mensual_periodo <- c(media_mensual_periodo, media_mes)
   }
 }
 
  #PROMEDIO SEMANAL
-media_semanal_xanio = c()
-for (i in 1:length(anios)) {
-  for (j in 1:length(meses)) {
-    anio = anios[i]
-    datos_anio = datos[datos$Anio == anio,]
-    datos_mes = datos_anio[datos_anio$Mes == j,]
+media_semanal_periodo = c()
+for (i in 1:length(anios_periodo)) {
+  for (j in 1:length(meses1)) {
+    anio <- anios_periodo[i]
+    datos_anio <- datos_periodo[datos_periodo$Anio == anio,]
+    datos_mes <- datos_anio[datos_anio$Mes == j,]
     for (k in seq(1,31,8)) {
-      semana = datos_mes[datos_mes$Dia %in% k:(k+7),]
-      media_semanal = round(mean(semana$CO2_ppm, na.rm = T),2)
-      media_semanal_xanio = c(media_semanal_xanio, media_semanal)
+      semana <- datos_mes[datos_mes$Dia %in% k:(k+7),]
+      media_semanal <- round(mean(semana$CO2_ppm, na.rm = T), 2)
+      media_semanal_periodo <- c(media_semanal_periodo, media_semanal)
     }
   }
 }
 
+#Agrego las medias al data frame
+datos_periodo$Media_mensual <- 0
+datos_periodo$Media_semanal <- 0
 
-#Armo el grafico pedido con ggplot
+k <- 0
+for (i in 1:length(anios_periodo)) {
+ anio <- anios_periodo[i]
+ for (j in 1:length(meses1)) {
+   datos_periodo$Media_mensual[
+    datos_periodo$Anio == anio & datos_periodo$Mes == j] <-
+    media_mensual_periodo[j+k*12]
+ }
+ k <- k + 1
+}
+
+sec <- seq(1,31,8)
+h <- 0
+for (i in 1:length(anios_periodo)) {
+  anio <- anios_periodo[i]
+  for (j in 1:length(meses1)) {
+    for (k in 1:length(sec)) {
+      dia <- sec[k]
+      datos_periodo$Media_semanal[
+       datos_periodo$Anio == anio & datos_periodo$Mes == j &
+        datos_periodo$Dia %in% dia:(dia+7)] <- media_semanal_periodo[k+h*4]
+    }
+    h <- h + 1
+  }
+}
+
+#Armo el grafico pedido con ggplot #####geom_ribbon()
 require(ggplot2)
-g = ggplot(data = datos, mapping = aes(x = Fecha, y = CO2_ppm)) 
-g = g + geom_point() 
-g = g + scale_x_date(date_breaks = "4 years", date_labels = "%Y-%m")
+datos_periodo$Etiqueta <- month(datos_periodo$Mes,label = T)
+datos_periodo$Etiqueta[which(datos_periodo$Dia != 15)] <- NA
+datos_periodo$Media_corrida <- datos_periodo$Media_mensual + 0.3
+
+g = ggplot(data = datos_periodo, mapping = aes(x = Fecha, y = CO2_ppm)) +
+  geom_point(color = "dark grey") +
+  scale_x_date(date_breaks = "2 month", date_labels = "%Y-%m") + 
+  geom_point(mapping = aes(x = Fecha, y = Media_mensual),
+                   shape = 0, color = "dark blue", size = 0.4) +
+  geom_text(datos_periodo,
+            mapping = aes(label = Etiqueta,
+                          x = Fecha, y = Media_corrida), color = "blue") +
+  geom_point(mapping = aes(x = Fecha, y = Media_semanal), shape = 0,
+             color = "violet", size = 0.3)
 g
-
-
-
-
 
 
 
